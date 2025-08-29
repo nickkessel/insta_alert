05524429ec0a8d6ca75506154b8f56729da1b5d5
Nick <nickkessel22@protonmail.com>
2025-08-29 12:05:50 -0400

    Fix #1: in addition to refactoring the pdsBox creation logic, it now gets plotted on top of map elements. sorting out the zorder of all elements, to remove overlap; clean up get_mrms_data; adjust position of radar_time; shrink path effect of text; generally readability changes; improved resilence for caption generation when it gets 'nulls'

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ test_bbox = {
         "lat_max": 55
 }
 
-warning_types = ["Tornado Warning", "Severe Thunderstorm Warning", "Flash Flood Warning"] 
+warning_types = ["Tornado Warning", "Severe Thunderstorm Warning", "Flash Flood Warning", "Special Weather Statement", "Special Marine Warning"] 
 
 # Store already posted alerts to prevent duplicates
 posted_alerts = set()
